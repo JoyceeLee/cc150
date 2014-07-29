ab604c48cc1461944d8878294d2e7728cf7ea87b
YangLi <liang.900503@gmail.com>
2014-07-29 00:07:19 -0700

    Create 01 Arrays and Strings p6.java

---
+++ 01 Arrays and Strings p6.java
@@ -0,0 +1,56 @@
+/*
+ * 1.6 Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, 
+ * write a method to rotate the image by 90 degrees. Can you do this in place?
+ */
+ 
+for(int i=0; i<N/2; i++) {
+    for(j=i; j<N-i; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[j][n-1-i];
+        matrix[j][n-1-i] = matrix[n-1-i][n-1-j];
+        matrix[n-1-i][n-1-j] = matrix[n-1-j][i];
+        matrix[n-1-j][i] = tmp;
+    }
+}
+ 
+ 
+ 
+public class Solution {
+	public static void main(String[] args) {
+	    int[][] A = new int[4][4];
+	    for(int i=0; i<4; i++)
+		for(int j=0; j<4; j++) {
+		    A[i][j] = (int) (Math.random() * 100);
+		}
+	    rotate(A);
+	}
+	public static void rotate(int[][] img) {
+	    int N = img.length;
+	    for(int j=0; j<N; j++) {
+	    	for(int jj=0; jj<N; jj++){
+	    		System.out.print(img[j][jj]+", ");
+	        }
+	    	System.out.print('\n');
+	    }
+	    System.out.print('\n');
+ 
+	    int n = img.length;
+            if(n<=1) return;
+            for(int i=0; i<(n/2);i++){
+                for(int j=i; j<n-i-1; j++){
+                    int tmp = img[i][j];
+                    img[i][j] = img[n-1-j][i];
+                    img[n-1-j][i] = img[n-1-i][n-1-j];
+                    img[n-1-i][n-1-j] = img[j][n-1-i];
+                    img[j][n-1-i] = tmp;
+                }
+            }
+        
+	    for(int j=0; j<N; j++) {
+	    	for(int jj=0; jj<N; jj++){
+	    	    System.out.print(img[j][jj]+", ");
+	        }
+	    	System.out.print('\n');
+	    }
+    }
+}
