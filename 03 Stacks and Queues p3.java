5995a60819fde8477436b85a507053124f17ea43
YangLi <liang.900503@gmail.com>
2014-07-31 09:40:24 -0700

    Update 03 Stacks and Queues p3.java

--- 03 Stacks and Queues p3.java
+++ 03 Stacks and Queues p3.java
@@ -1,7 +1,7 @@
 /*
  * 3.3 Imagine a (literal) stack of plates. If the stack gets too high, it might topple. 
  * Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. 
- * Implement a data structureSetOf Stacks that mimics this. 
+ * Implement a data structure SetOf Stacks that mimics this. 
  * SetOf Stacks should be composed of several stacks and should create a new stack once the previous one exceeds capacity. 
  * SetOfStacks.push() and SetOfStacks.pop() should be have identically to a single stack(that is,pop() should 
  * return the same values as it would if there were just a single stack).
